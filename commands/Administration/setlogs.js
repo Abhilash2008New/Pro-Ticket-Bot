489e5fe70d039268e91fd3ec71c47e6573735344
Abhilash2008New <83767306+Abhilash2008New@users.noreply.github.com>
2021-06-22 14:23:06 +0530

    Update setlogs.js

--- commands/Administration/setlogs.js
+++ commands/Administration/setlogs.js
@@ -7,23 +7,23 @@ const color = JSON.parse(fs.readFileSync(`Storage/color.json`, `utf8`));
 exports.run = async (bot, message, args, functions) => {
 
 let channel = message.mentions.channels.first();
-if(!channel || channel.type !== "text") return functions.errorEmbed(message, message.channel, "Veuillez un salon texte.");
+if(!channel || channel.type !== "text") return functions.errorEmbed(message, message.channel, "Please a living room text.");
 
 let channelFetched = message.guild.channels.cache.find(c => c.id === channel.id);
-if(!channelFetched || channelFetched.type !== "text") return functions.errorEmbed(message, message.channel, "Veuillez un salon texte.");
+if(!channelFetched || channelFetched.type !== "text") return functions.errorEmbed(message, message.channel, "Please a living room text.");
 
 let embed = new Discord.MessageEmbed()
-.setAuthor(`✅ | Salon Définit`)
+.setAuthor(`✅ | Salon Sets`)
 .setColor(color.none)
 .setTimestamp()
 .setFooter(`Ticket System`, bot.user.displayAvatarURL())
-.addField(`Salon définit`, channelFetched, true)
-.addField(`Définit par`, message.author, true)
+.addField(`Salon sets`, channelFetched, true)
+.addField(`Defined by`, message.author, true)
 .addField(`Date`, `\`${dateFormat(new Date(), "dd/mm/yyyy - HH:MM:ss")}\``, true);
 
 db.set(`logs_${message.guild.id}`, channelFetched.id);
 channelFetched.send(message.author, {embed: embed});
-functions.successEmbed(message, message.channel, `Le salon de \`logs\` a été défini sur ${channelFetched}`);
+functions.successEmbed(message, message.channel, `The \`logs\` lounge has been set to ${channelFetched}`);
 
 }
 
