d66fd0bae4080b449a68a99262f9754c000aba60
Abhilash2008New <83767306+Abhilash2008New@users.noreply.github.com>
2021-06-22 14:28:03 +0530

    Update functions.js

--- functions/functions.js
+++ functions/functions.js
@@ -32,11 +32,11 @@ module.exports = {
   // =================================== EMBED SUCCESS & ERROR =================================== //
 
   errorEmbed: function(message, channel, argument) {
-    channel.send(new Discord.MessageEmbed().setDescription(`\\📛 **Erreur:** ${argument} \\📛`).setColor(color.red))
+    channel.send(new Discord.MessageEmbed().setDescription(`\\📛 **Error:** ${argument} \\📛`).setColor(color.red))
   },
 
   successEmbed: function(message, channel, argument) {
-    channel.send(new Discord.MessageEmbed().setDescription(`\\✅ **Succès:** ${argument}`).setColor(color.green))
+    channel.send(new Discord.MessageEmbed().setDescription(`\\✅ **Success:** ${argument}`).setColor(color.green))
   },
 
 }
